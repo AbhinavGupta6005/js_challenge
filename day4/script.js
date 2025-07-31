7eac94b0bf4b76941865417fdbc463ba200ca71a
AbhinavGupta6--5 <motilalg25@gmail.com>
2025-07-31 20:32:15 +0530

    Text Counter

---
+++ day4/script.js
@@ -0,0 +1,10 @@
+const textarea = document.querySelector('textarea')
+const counter = document.querySelector('.counter')
+
+
+function count(){
+    const text = textarea.value;
+    const length = textarea.value.length;
+
+    counter.innerText=`${length}`;;
+}
