1afe7fba6e8c0f71e27c3e7336ffa2cf0fe8764c
AbhinavGupta6--5 <motilalg25@gmail.com>
2025-08-05 19:10:20 +0530

    Add & Remove

---
+++ day9/script.js
@@ -0,0 +1,29 @@
+let survey_options = document.getElementById('survey_options')
+let add = document.getElementById('add_fields')
+let  remove = document.getElementById('remove_fields')
+
+add.onclick = function(){
+    let field = document.createElement('input')
+    var tags = survey_options.getElementsByTagName('input')
+
+
+    field.setAttribute('type','text')
+    field.setAttribute('name','survey_options')
+    field.setAttribute('class','survey_options')
+    field.setAttribute('size','50')
+
+    survey_options.appendChild(field)
+    if(tags.length >= 2){
+        document.getElementById('remove_fields').style.visibility = 'visible'
+    }
+}
+
+remove.onclick = function(){
+    let tags = survey_options.getElementsByTagName('input')
+    if(tags.length >= 2){
+        survey_options.removeChild(tags[(tags.length)-1]);
+        if(tags.length < 2){
+            document.getElementById('remove_fields').style.visibility = 'hidden'
+        }
+    }
+}
